65e35792d9d657982232944c25e37e59fd3eb26f
Kwame <1kwameearl@gmail.com>
2023-08-10 16:55:49 -0700

    Escaping a comma to get the correct output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/*
+ *This program prints out a specific 
+ * ouput
+*/
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
