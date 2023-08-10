9e090c0546798279a16e41bd9c964655c6b2a4c2
Kwame <1kwameearl@gmail.com>
2023-08-10 16:58:26 -0700

    Betty documentation style fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/*
- *This program prints out a specific 
- * ouput
-*/
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
