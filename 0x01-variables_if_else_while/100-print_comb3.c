08bbb19e7eb4c92b90a3c93d8fecbd13cff3d69c
Kwame <1kwameearl@gmail.com>
2023-08-14 13:48:32 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Print a combination of 99 and 89
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 48;
+	int m = 49;
+
+	while (n <= 56)
+	{
+		while (m <= 57)
+		{
+			if (m > n)
+			{
+				putchar('n');
+				putchar('m');
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			m++;
+		}
+		n++;
+		m = 49;
+	}
+	putchar('\n');
+	return (0);
+}
