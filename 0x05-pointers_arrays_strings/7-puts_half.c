1b1458682b3e562e5fe7317b5dc94240b3219ee3
Kwame <1kwameearl@gmail.com>
2023-08-22 16:26:24 -0700

    7- 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * puts_half - prints second half of string
+ * @str: pointer to the string
+ *
+ * Return: string
+ */
+
+void puts_half(char *str)
+{
+	int l;
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	if (i % 2 == 0)
+	{
+		for (l = i / 2; str[l] != '\0'; l++)
+		{
+			_putchar(str[l]);
+		}
+	}
+	else
+	{
+		for (l = (i - 1) / 2; str[l] != '\0'; l++)
+		{
+			_putchar(str[l]);
+		}
+	}
+	_putchar('\n');
+}
+
