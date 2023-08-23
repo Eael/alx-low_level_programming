dfdd7b6f8b91b00a9dc64c8ec92b5dfcc5b539d7
Kwame <1kwameearl@gmail.com>
2023-08-22 17:25:36 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,51 @@
+#include "main.h"
+/**
+ * _atoi - takes a pointer to a string and converts it to an integer
+ * @s: pointer to string we want to convert
+ *
+ * Return: converted integert
+ */
+
+int _atoi(char *s)
+{
+	int a, b, c, d, len, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	len = 0;
+	digit = 0;
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	while (a < len && d == 0)
+	{
+		if (s[a] == 45)
+		{
+			++b;
+		}
+		if (s[a] >= 48 && s[a] <= 57)
+		{
+			digit = s[a] - 48;
+			if (b % 2)
+			{
+				digit = -digit;
+			}
+			c = c * 10 + digit;
+			d = 1;
+			if (s[a + 1] < 48 || s[a + 1] > 57)
+			{
+				break;
+			}
+			f = 0
+		}
+		a++;
+	}
+	if (d == 0)
+	{
+		return (0);
+	}
+	return (c);
+}
