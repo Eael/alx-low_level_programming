b9642fc2790a55f31efdb956a1ffa0e800efb45b
Kwame <1kwameearl@gmail.com>
2023-08-22 17:05:05 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcpy - takes a pointer to a string and copies it to a dest
+ * @src: pointer to string we want to print
+ * @dest: pointer to dest
+ *
+ * Return: the pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int l;
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		i++
+	}
+	for (l = 0; l < i; l++)
+	{
+		dest[l] = src[l];
+	}
+	dest[l] = '\0';
+
+	return (dest);
+}
+
