97688350155905054d09a1bdc8e819bb167d7aa3
Kwame <1kwameearl@gmail.com>
2023-08-25 07:03:51 -0700

    55-str

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - Changes all lowercase to upper case
+ *
+ * Return: Upper case.
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+	return (s);
+}
