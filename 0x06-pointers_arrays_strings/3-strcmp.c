c68e7e4a881cd228016517a6dbad5ae734138417
Kwame <1kwameearl@gmail.com>
2023-08-25 03:58:57 -0700

    3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns positive or negative number
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int res;
+
+	res = s1 - s2;
+	return (res);
+}
