03b5eb75d26014eb935093b5d2242b84bd7b6081
Eael <teyeearl@gmail.com>
2023-09-07 12:29:25 +0000

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,7 +3,7 @@
  * *str_concat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * 
+ *
  * Return: pointer to newly allocated space or NULL if failed
 */
 char *str_concat(char *s1, char *s2)
@@ -11,31 +11,40 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, len, k;
 	char *temp;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 != NULL)
 	{
-		return (NULL);
+		while (s1[j] != '\0')
+		{
+			j++;
+		}
 	}
-	while (s2[i] != '\0')
+	if (s2 != NULL)
 	{
-		i++;
+		while (s2[i] != '\0')
+		{
+			i++;
+		}
 	}
-	while (s1[j] != '\0')
-	{
-		j++;
-	}
+
 	len = i + j;
 	temp = malloc(sizeof(char) * (len + 1));
 	if (temp == NULL)
 	{
 		return (NULL);
 	}
-	for (k = 0; k < j; k++)
+	if (s1 != NULL)
 	{
-		temp[k] = s1[k];
+		for (k = 0; k < j; k++)
+		{
+			temp[k] = s1[k];
+		}
 	}
-	for (i = 0; i <= len; i++)
+	if (s2 != NULL)
 	{
-		temp[k + i] = s2[i];
+		for (i = 0; i <= len; i++)
+		{
+			temp[k + i] = s2[i];
+		}
 	}
 	return (temp);
 }
