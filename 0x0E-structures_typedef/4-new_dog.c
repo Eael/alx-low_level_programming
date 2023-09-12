d9aabfeb6ba6944adb13b3083725a0c76d09ac24
Eael <teyeearl@gmail.com>
2023-09-12 14:17:50 +0000

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,13 +22,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	(*create_dog).name = _strdup(name);
 	(*create_dog).owner = _strdup(owner);
 
-	if (create_dog->name == NULL || create_dog->owner == NULL) 
-	{
-    	free(create_dog->name);
-        free(create_dog->owner);
-        free(create_dog);
-        return NULL;
-    }
 	(*create_dog).age = age;
 
 	return (create_dog);
