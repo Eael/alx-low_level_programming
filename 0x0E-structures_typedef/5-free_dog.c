b9bf058448c3e04b77c1cc79d4c2e7858c164a79
Eael <teyeearl@gmail.com>
2023-09-12 00:28:48 +0000

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * free_dog -
+ * @d: pointer to dog
+ * 
+ * Return: nothing
+*/
+
+void free_dog(dog_t *d)
+{
+	if (d->name != NULL && d->owner != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
