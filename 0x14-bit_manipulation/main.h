f64fd156b57f11cedced8b18726aeb145a78257c
Kwame <1kwameearl@gmail.com>
2023-09-29 09:22:28 -0700

    All commits

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_h
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,7 +7,7 @@
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
-
+void print_binary(unsigned long int n);
 
 
 
