ee93128211ab06dd4cdb1df2923532ea212b0c72
Kwame <1kwameearl@gmail.com>
2023-09-29 09:04:56 -0700

    Main.h

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_h
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
