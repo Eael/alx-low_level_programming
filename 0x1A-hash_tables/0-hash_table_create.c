b2157948f3f5c5b96b92b476f6d24688b565ac09
Kwame <1kwameearl@gmail.com>
2023-12-06 12:30:09 -0800

    betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,10 +7,10 @@
  * Return: pointer to newly created hash table
  */
 /*
-1. Allocate memory for the hash table.
-2. Allocate memory for the array of pointers to the nodes.
-3. Initialize the array of pointers to the nodes.
-4. Return the pointer to the hash table.
+*1. Allocate memory for the hash table.
+*2. Allocate memory for the array of pointers to the nodes.
+*3. Initialize the array of pointers to the nodes.
+*4. Return the pointer to the hash table.
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
@@ -29,8 +29,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 	}
 	for (i = 0; i < size; i++)
-	{
 		hash_table->array[i] = NULL;
-	}
+
 	return (hash_table);
 }
